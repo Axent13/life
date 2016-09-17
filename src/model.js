5d5b6a8ddfe584b5b60251d1eed47bbd2718d768
Axent13 <Axent13@yandex.ru>
2016-09-17 20:25:07 +0700

    Issue #4 completed

--- src/model.js
+++ src/model.js
@@ -26,78 +26,48 @@ export class Model {
         }
     }
 
+    _isElementInsideField(i, j) {
+        if(j >= 0 && j < this._field_height){
+            if(i >= 0 && i < this._field_width){
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+
+    _checkingAliveNeighbours(i, j) {
+        let alive_neighbours = 0;
+
+        for(let y = j - 1; y <= j + 1; y++){
+            for(let x = i - 1; x <= i + 1; x++){
+                if(x === i && y === j){
+                    continue;
+                } else {
+                    if(this._isElementInsideField(x, y) && this._cells[x][y] === 1){
+                        alive_neighbours++;
+                    }
+                }
+            }
+        }
+
+        return alive_neighbours;
+    }
+
     nextCellStates() {
         let changing_cells = [];
 
         for(let i = 0; i < this._field_height; i++){
             for(let j = 0; j < this._field_width; j++){
-                let alive_neighbours = 0;
+                let alive_neighbours_counter = 0;
 
-                if (i === 0 && j === 0){
-                    //левый верхний угол
-                    this._cells[i][j + 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i + 1][j + 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i + 1][j] === 1 ? alive_neighbours++ : false;
-                } else if (i === 0 && j === (this._field_width - 1) ){
-                    //правый верхний угол
-                    this._cells[i][j - 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i + 1][j - 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i + 1][j] === 1 ? alive_neighbours++ : false;
-                } else if (i === (this._field_height - 1) && j === 0){
-                    //левый нижний угол
-                    this._cells[i - 1][j] === 1 ? alive_neighbours++ : false;
-                    this._cells[i - 1][j + 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i][j + 1] === 1 ? alive_neighbours++ : false;
-                } else if (i === (this._field_height - 1) && j === (this._field_width - 1)){
-                    //правый нижний угол
-                    this._cells[i - 1][j] === 1 ? alive_neighbours++ : false;
-                    this._cells[i - 1][j - 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i][j - 1] === 1 ? alive_neighbours++ : false;
-                } else if (i === 0){
-                    //верхняя строка
-                    this._cells[i][j - 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i + 1][j - 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i + 1][j] === 1 ? alive_neighbours++ : false;
-                    this._cells[i + 1][j + 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i][j + 1] === 1 ? alive_neighbours++ : false;
-                } else if (i === (this._field_height - 1) ){
-                    //нижняя строка
-                    this._cells[i][j - 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i - 1][j - 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i - 1][j] === 1 ? alive_neighbours++ : false;
-                    this._cells[i - 1][j + 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i][j + 1] === 1 ? alive_neighbours++ : false;
-                } else if (j === 0){
-                    //левый столбец
-                    this._cells[i - 1][j] === 1 ? alive_neighbours++ : false;
-                    this._cells[i - 1][j + 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i][j + 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i + 1][j + 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i + 1][j] === 1 ? alive_neighbours++ : false;
-                } else if (j === (this._field_width - 1)){
-                    //правый столбец
-                    this._cells[i - 1][j] === 1 ? alive_neighbours++ : false;
-                    this._cells[i - 1][j - 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i][j - 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i + 1][j - 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i + 1][j] === 1 ? alive_neighbours++ : false;
-                } else {
-                    //внутри поля
-                    this._cells[i - 1][j - 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i][j - 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i + 1][j - 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i + 1][j] === 1 ? alive_neighbours++ : false;
-                    this._cells[i + 1][j + 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i][j + 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i - 1][j + 1] === 1 ? alive_neighbours++ : false;
-                    this._cells[i - 1][j] === 1 ? alive_neighbours++ : false;
-                }
+                alive_neighbours_counter = this._checkingAliveNeighbours(i, j);
 
-                if(this._cells[i][j] === 0 && alive_neighbours === 3){
+                if(this._cells[i][j] === 0 && alive_neighbours_counter === 3){
                     changing_cells.push([i, j]);
-                } else if (this._cells[i][j] === 1 && alive_neighbours < 2){
+                } else if (this._cells[i][j] === 1 && alive_neighbours_counter < 2){
                     changing_cells.push([i, j]);
-                } else if (this._cells[i][j] === 1 && alive_neighbours > 3){
+                } else if (this._cells[i][j] === 1 && alive_neighbours_counter > 3){
                     changing_cells.push([i, j]);
                 }
             }
