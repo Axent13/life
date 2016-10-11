4445a418e1e475921a12bf486fcd9846fa15cc2a
Axent13 <Axent13@yandex.ru>
2016-10-11 18:04:04 +0700

    #16 исправил имя переменных

--- src/view.js
+++ src/view.js
@@ -1,15 +1,15 @@
 export default class View {
 
     constructor(fieldWidth = 30, fieldHeight = 30) {
-        this._field_width = fieldWidth;
-        this._field_height = fieldHeight;
+        this._fieldWidth = fieldWidth;
+        this._fieldHeight = fieldHeight;
     }
 
     drawField() {
         let resultingField = '';
-        for (let i = 0; i < this._field_height; i += 1) {
+        for (let i = 0; i < this._fieldHeight; i += 1) {
             resultingField += '<tr>';
-            for (let j = 0; j < this._field_width; j += 1) {
+            for (let j = 0; j < this._fieldWidth; j += 1) {
                 resultingField += `<td id=\'${i}-${j}\' class=\'dead\'></td>`;
             }
             resultingField += '</tr>';
